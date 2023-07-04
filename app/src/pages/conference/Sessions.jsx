a03d4e3712e55e046ae1d1754671777cc763f702
estebanvillegas <esteban.villegas@polestar.com>
2023-07-04 15:52:24 +0200

    used the useMutation hook together with Formik to create a new Session.

--- app/src/pages/conference/Sessions.jsx
+++ app/src/pages/conference/Sessions.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";  
+import React, { useState } from "react";
 import "./style-sessions.css";
 import { Link } from "react-router-dom"
 import { Formik, Field, Form } from "formik"
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, useMutation } from "@apollo/client";
 
 /* Define queries, mutations and fragments here */
 const SESSIONS_FRAGMENT = gql`
@@ -33,15 +33,24 @@ const SESSIONS_QUERY = gql`
       ...SessionInfo
     }
   }
-  ${ SESSIONS_FRAGMENT }
+  ${SESSIONS_FRAGMENT}
+`;
+
+const CREATE_SESSION_MUTATION = gql`
+  mutation createSession($session: SessionInput!) {
+    createSession(session: $session) {
+      id
+      title
+    }
+  }
 `;
 
 function AllSessionList() {
-   /* ---> Invoke useQuery hook here to retrieve all sessions and call SessionItem */
-   return <SessionItem />
+  /* ---> Invoke useQuery hook here to retrieve all sessions and call SessionItem */
+  return <SessionItem />
 }
 
-function SessionList ({ day }) {
+function SessionList({ day }) {
   if (day == "") {
     day = "Wednesday";
   }
@@ -58,21 +67,21 @@ function SessionList ({ day }) {
   const results = [];
 
   results.push(data.intro.map((session) => (
-    <SessionItem 
+    <SessionItem
       key={session.id}
-      session={{...session}}/>
+      session={{ ...session }} />
   )));
 
   results.push(data.intermediate.map((session) => (
-    <SessionItem 
+    <SessionItem
       key={session.id}
-      session={{...session}}/>
+      session={{ ...session }} />
   )));
 
   results.push(data.advanced.map((session) => (
-    <SessionItem 
+    <SessionItem
       key={session.id}
-      session={{...session}}/>
+      session={{ ...session }} />
   )));
 
   return results;
@@ -91,16 +100,16 @@ function SessionItem({ session }) {
           <h5>{`Day: ${day}`}</h5>
           <h5>{`Room Number: ${room}`}</h5>
           <h5>{`Starts at: ${startsAt}`}</h5>
-          { description && <p>{ description }</p> }
+          {description && <p>{description}</p>}
         </div>
         <div className="panel-footer">
-          { speakers.map(({ id, name }) => (
+          {speakers.map(({ id, name }) => (
             <span key={id} style={{ padding: 2 }}>
               <Link className="btn btn-default btn-lg" to={`/conference/speaker/${id}`}>
                 View {name}'s profile
               </Link>
             </span>
-          )) }
+          ))}
         </div>
       </div>
     </div>
@@ -113,122 +122,126 @@ export function Sessions() {
     <>
       <section className="banner">
         <div className="container">
-        <div className="row" style={{ padding: 10 }}>	
-            <Link	
-              className="btn btn-lg center-block"	
-              to={`/conference/sessions/new`}	
-            >	
-              Submit a Session!	
-            </Link>	
+          <div className="row" style={{ padding: 10 }}>
+            <Link
+              className="btn btn-lg center-block"
+              to={`/conference/sessions/new`}
+            >
+              Submit a Session!
+            </Link>
           </div>
           <div className="row">
-          <button type="button" onClick={() => setDay('All')} className="btn-oval">
+            <button type="button" onClick={() => setDay('All')} className="btn-oval">
               All Sessions
             </button >
             <button type="button" onClick={() => setDay('Wednesday')} className="btn-oval">
               Wednesday
             </button>
-            <button type="button"  onClick={() => setDay('Thursday')} className="btn-oval">
+            <button type="button" onClick={() => setDay('Thursday')} className="btn-oval">
               Thursday
             </button>
             <button type="button" onClick={() => setDay('Friday')} className="btn-oval">
               Friday
             </button >
           </div>
-          { day !== 'All' && <SessionList day={day} />}
-          { day === 'All' && <AllSessionList /> }
+          {day !== 'All' && <SessionList day={day} />}
+          {day === 'All' && <AllSessionList />}
         </div>
       </section>
     </>
   );
 }
 
-export function SessionForm() {	
+export function SessionForm() {
 
-  /* ---> Call useMutation hook here to create new session and update cache */
+  const [createSession, data] = useMutation(CREATE_SESSION_MUTATION);
 
-  return (	
-    <div	
-      style={{	
-        width: "100%",	
-        display: "flex",	
-        alignContent: "center",	
-        justifyContent: "center",	
-        padding: 10,	
-      }}	
-    >	
-      <Formik	
-        initialValues={{	
-          title: "",	
-          description: "",	
-          day: "",	
-          level: "",	
-        }}	
-        onSubmit={() => {
-          /* ---> Call useMutation mutate function here to create new session */
-        }}	
-      >	
-        {() => (	
-          <Form style={{ width: "100%", maxWidth: 500 }}>	
-            <h3 className="h3 mb-3 font-weight-normal">Submit a Session!</h3>	
-            <div className="mb-3" style={{ paddingBottom: 5 }}>	
-              <label htmlFor="inputTitle">Title</label>	
-              <Field	
-                id="inputTitle"	
-                className="form-control"	
-                required	
-                autoFocus	
-                name="title"	
-              />	
-            </div>	
-            <div className="mb-3" style={{ paddingBottom: 5 }}>	
-              <label htmlFor="inputDescription">Description</label>	
-              <Field	
-                type="textarea"	
-                id="inputDescription"	
-                className="form-control"	
-                required	
-                name="description"	
-              />	
-            </div>	
-            <div className="mb-3" style={{ paddingBottom: 5 }}>	
-              <label htmlFor="inputDay">Day</label>	
-              <Field	
-                name="day"	
-                id="inputDay"	
-                className="form-control"	
-                required	
-              />	
-            </div>	
-            <div className="mb-3" style={{ paddingBottom: 5 }}>	
-              <label htmlFor="inputLevel">Level</label>	
-              <Field	
-                name="level"	
-                id="inputLevel"	
-                className="form-control"	
-                required	
-              />	
-            </div>	
-            <div style={{ justifyContent: "center", alignContent: "center" }}>
-              <button className="btn btn-primary">Submit</button>	
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        alignContent: "center",
+        justifyContent: "center",
+        padding: 10,
+      }}
+    >
+      <Formik
+        initialValues={{
+          title: "",
+          description: "",
+          day: "",
+          level: "",
+        }}
+        onSubmit={async (values) => {
+          await createSession({
+            variables: {
+              session: values
+            }
+          });
+        }}
+      >
+        {() => (
+          <Form style={{ width: "100%", maxWidth: 500 }}>
+            <h3 className="h3 mb-3 font-weight-normal">Submit a Session!</h3>
+            <div className="mb-3" style={{ paddingBottom: 5 }}>
+              <label htmlFor="inputTitle">Title</label>
+              <Field
+                id="inputTitle"
+                className="form-control"
+                required
+                autoFocus
+                name="title"
+              />
             </div>
-          </Form>	
-        )}	
-      </Formik>	
-    </div>	
-  );	
+            <div className="mb-3" style={{ paddingBottom: 5 }}>
+              <label htmlFor="inputDescription">Description</label>
+              <Field
+                type="textarea"
+                id="inputDescription"
+                className="form-control"
+                required
+                name="description"
+              />
+            </div>
+            <div className="mb-3" style={{ paddingBottom: 5 }}>
+              <label htmlFor="inputDay">Day</label>
+              <Field
+                name="day"
+                id="inputDay"
+                className="form-control"
+                required
+              />
+            </div>
+            <div className="mb-3" style={{ paddingBottom: 5 }}>
+              <label htmlFor="inputLevel">Level</label>
+              <Field
+                name="level"
+                id="inputLevel"
+                className="form-control"
+                required
+              />
+            </div>
+            <div style={{ justifyContent: "center", alignContent: "center" }}>
+              <button className="btn btn-primary">Submit</button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
-export function AddSession() {	
-  return (	
-    <>	
-      <section className="banner">	
-        <div className="container">	
-          <div className="row">	
-            <SessionForm />	
-          </div>	
-        </div>	
-      </section>	
-    </>	
-  );	
+export function AddSession() {
+  return (
+    <>
+      <section className="banner">
+        <div className="container">
+          <div className="row">
+            <SessionForm />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
